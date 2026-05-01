9961d7bbd38577901faf249d1e983a34a165570d
Shreya Narayani <narayanishreya7@gmail.com>
2026-05-01 12:03:16 +0530

    maybe

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Hero from '../components/sections/Hero/Hero';
+import Collections from '../components/sections/Collections/Collections';
+import Heritage from '../components/sections/Heritage/Heritage';
 import Featured from '../components/sections/Featured/Featured';
 import Flagship from '../components/sections/Flagship/Flagship';
 import Testimonials from '../components/sections/Testimonials/Testimonials';
@@ -9,6 +11,8 @@ const HomePage: React.FC = () => {
   return (
     <div className="page-home">
       <Hero />
+      <Collections />
+      <Heritage />
       <Featured />
       <Flagship />
       <Testimonials />
